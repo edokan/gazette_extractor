dc1a277fd8cd847d45e77f7511417f00ed9e8491
Nefertari <s396289@pc22i>
2016-04-05 19:48:16 +0200

    Improved extractor to python3, used argparse to improve loading parameters, also made a ind of help in input

--- extractor.py
+++ extractor.py
@@ -1,21 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import xml.etree.ElementTree as ET, lxml.etree, re, sys
+import xml.etree.ElementTree as ET, lxml.etree, sys, argparse
 
-tree = ET.parse(sys.argv[1])
+parser = argparse.ArgumentParser(description="Extractor of features included in .xml file od djvu format")
+parser.add_argument('path_to_file', help = "Path to .xml file produced from djvu")
+args = parser.parse_args()
+tree = ET.parse(args.path_to_file)
 root = tree.getroot()
 
-#Wyrzucanie współrzędnych słów jako krotka krotek
 for word in root.iter('WORD'):
-    coordinates = word.attrib.values()[0].split(',')
-    #"dół" słowa
-    x1y1 = [coordinates[0], coordinates[1]]
-    x2y1 = [coordinates[2], coordinates[1]]
-    #"góra" słowa
-    x1y2 = [coordinates[0], coordinates[3]]
-    x2y2 = [coordinates[2], coordinates[3]]
-
+    coordinates = list(word.attrib.values())[0].split(',')
+    x1 = coordinates[0]
+    y1 = coordinates[1]
+    x2 = coordinates[2]
+    y2 = coordinates[3]
     height = float(coordinates[1]) - float(coordinates[3])
     width = float(coordinates[2]) - float(coordinates[0])
-
-    print [word.text, x1y1, x2y1, x1y2, x2y2, height, width]
+    print ("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" %(word.text, x1, y1, x2, y1, x1, y2, x2, y2, height, width))
