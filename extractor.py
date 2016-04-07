4f46959dbfdb45ec474277e1c1c60aace2dec072
fiqas <salimaire@gmail.com>
2016-04-07 13:29:20 -0700

    Zmodyfikowano extractor.py

--- extractor.py
+++ extractor.py
@@ -3,17 +3,19 @@
 import xml.etree.ElementTree as ET, lxml.etree, sys, argparse
 
 parser = argparse.ArgumentParser(description="Extractor of features included in .xml file od djvu format")
-parser.add_argument('path_to_file', help = "Path to .xml file produced from djvu")
+parser.add_argument('-p', help = "Path to .xml file produced from djvu")
 args = parser.parse_args()
-tree = ET.parse(args.path_to_file)
+
+tree = ET.parse(args.p)
 root = tree.getroot()
 
-for word in root.iter('WORD'):
+for word in root.iter('CHARACTER'):
     coordinates = list(word.attrib.values())[0].split(',')
     x1 = coordinates[0]
     y1 = coordinates[1]
     x2 = coordinates[2]
     y2 = coordinates[3]
-    height = float(coordinates[1]) - float(coordinates[3])
-    width = float(coordinates[2]) - float(coordinates[0])
-    print ("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" %(word.text, x1, y1, x2, y1, x1, y2, x2, y2, height, width))
+    height = int(coordinates[1]) - int(coordinates[3])
+    width = int(coordinates[2]) - int(coordinates[0])
+    print("\t".join([str(x1), str(y1), str(x2), str(y2)]))
+    # print ("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" %(word.text, x1, y1, x2, y1, x1, y2, x2, y2, height, width))
