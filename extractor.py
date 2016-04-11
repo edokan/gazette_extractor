6513b55ae815cf51a31b71a0596eede23fda6add
Nefertari <s396289@pc22i>
2016-04-11 14:59:03 +0200

    update of extractor

--- extractor.py
+++ extractor.py
@@ -9,13 +9,11 @@ args = parser.parse_args()
 tree = ET.parse(args.p)
 root = tree.getroot()
 
-for word in root.iter('CHARACTER'):
-    coordinates = list(word.attrib.values())[0].split(',')
+for word in root.iter('WORD'):
+    if not word: coordinates = list(word.attrib.values())[0].split(',')
     x1 = coordinates[0]
     y1 = coordinates[1]
     x2 = coordinates[2]
     y2 = coordinates[3]
-    height = int(coordinates[1]) - int(coordinates[3])
-    width = int(coordinates[2]) - int(coordinates[0])
     print("\t".join([str(x1), str(y1), str(x2), str(y2)]))
-    # print ("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" %(word.text, x1, y1, x2, y1, x1, y2, x2, y2, height, width))
+    
