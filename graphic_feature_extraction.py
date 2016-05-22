2cf2ad41fc67b9261a7003d441a16cf612f0efee
fiqas <salimaire@gmail.com>
2016-05-22 07:07:25 -0700

    Added histogram in graphic features, modified rectangle script to output feature names

--- graphic_feature_extraction.py
+++ graphic_feature_extraction.py
@@ -1,14 +1,16 @@
 import sys
 import cv2
 import argparse
-from collections import defaultdict
+from collections import OrderedDict
+import numpy as np
+from detect_peaks import detect_peaks
 
 parser = argparse.ArgumentParser(description = "")
 parser.add_argument("-f", help = "Page image input")
 parser.add_argument("-v", help = "Verbose mode", action = 'store_true', default = False)
 args = parser.parse_args()
 
-features = {}
+features = OrderedDict()
 
 
 def histograms(roi):
@@ -16,18 +18,19 @@ def histograms(roi):
     horizontal = []
     vertical = []
 
-    roi_histograms = cv2.cvtColor(roi, cv2.COLOR_GRAY2RGB)
+    horizontal_hist = cv2.cvtColor(roi, cv2.COLOR_GRAY2RGB)
+    vertical_hist = cv2.cvtColor(roi, cv2.COLOR_GRAY2RGB)
     
     for i in range(height):
         value = width - cv2.countNonZero(roi[i, :])
         if value == width:
             value = width - 1
         vertical.append(value)
-        roi_histograms[i][value] = [0, 0, 255] 
+        vertical_hist[i][value] = [0, 0, 255] 
         if i == 0:
             before = (value, i)
         else:
-            cv2.line(roi_histograms, before, (value, i), (0, 0, 255), 2)
+            cv2.line(vertical_hist, before, (value, i), (0, 0, 255), 2)
             before = (value, i)
 
     for i in range(width):
@@ -35,15 +38,51 @@ def histograms(roi):
         if value == height:
             value = height - 1
         horizontal.append(value)
-        roi_histograms[value][i] = [255, 0, 0] 
+        horizontal_hist[value][i] = [255, 0, 0] 
         if i == 0:
             before = (i, value)
         else:
-            cv2.line(roi_histograms, before, (i, value), (255, 0, 0), 2)
+            cv2.line(horizontal_hist, before, (i, value), (255, 0, 0), 2)
             before = (i, value)
+
+    ############# HISTOGRAM PEAKS AND VALLEYS #######################################33
     
+    every = 4
+
+    horizontal_less = horizontal[0::every]
+    vertical_less = vertical[0::every]
+
+    ### VALLEYS ###
+
+    horizontal_valleys = detect_peaks(horizontal_less, mph=None, mpd=10, threshold=0, edge='falling', kpsh=False, valley=True, show=False, ax=None)
+    for i in range(len(horizontal_valleys)):
+        cv2.line(horizontal_hist, (horizontal_valleys[i] * every, horizontal[horizontal_valleys[i] * every]), (horizontal_valleys[i] * every, horizontal[horizontal_valleys[i] * every]), (255, 255, 0), 10)
+
+    vertical_valleys = detect_peaks(vertical_less, mph=None, mpd=10, threshold=0, edge='falling', kpsh=False, valley=True, show=False, ax=None)
+    for i in range(len(vertical_valleys)):
+        cv2.line(vertical_hist, (vertical[vertical_valleys[i] * every], vertical_valleys[i] * every), (vertical[vertical_valleys[i] * every], vertical_valleys[i] * every), (255, 255, 0), 10)
+
+    ### PEAKS ###
+    
+    horizontal_peaks = detect_peaks(horizontal_less, mph=None, mpd=10, threshold=0, edge='falling', kpsh=False, valley=False, show=False, ax=None)
+    for i in range(len(horizontal_peaks)):
+        cv2.line(horizontal_hist, (horizontal_peaks[i] * every, horizontal[horizontal_peaks[i] * every]), (horizontal_peaks[i] * every, horizontal[horizontal_peaks[i] * every]), (0, 255, 0), 10)
+
+    vertical_peaks = detect_peaks(vertical_less, mph=None, mpd=10, threshold=0, edge='falling', kpsh=False, valley=False, show=False, ax=None)
+    for i in range(len(vertical_peaks)):
+        cv2.line(vertical_hist, (vertical[vertical_peaks[i] * every], vertical_peaks[i] * every), (vertical[vertical_peaks[i] * every], vertical_peaks[i] * every), (0, 255, 0), 10)
+
+    ### SHOW HISTOGRAMS IN VERBOSE MODE ###
+
+    features["HORIZONTAL_PEAKS"] = str(len(horizontal_peaks))
+    features["HORIZONTAL_VALLEYS"] = str(len(horizontal_valleys))
+    features["VERTICAL_PEAKS"] = str(len(vertical_peaks))
+    features["VERTICAL_VALLEYS"] = str(len(vertical_valleys))
+
+
     if args.v:
-        cv2.imshow('histograms', roi_histograms)
+        cv2.imshow('horizontal', horizontal_hist)
+        cv2.imshow('vertical', vertical_hist)
         cv2.waitKey()
 
 
@@ -74,7 +113,7 @@ def print_features():
 if __name__ == "__main__":
     rectangles = []
     for line in sys.stdin:
-        x1, y1, x2, y2 = [int(x) for x in line.strip().split()]
+        x1, y1, x2, y2 = [int(x.split(":")[1]) for x in line.strip().split()]
         rectangles.append((x1, y1, x2, y2))
 
     page = cv2.imread(args.f, 0)
--- rectangle.py
+++ rectangle.py
@@ -26,5 +26,9 @@ if __name__ == "__main__":
 
     for i in range(len(steps_h) - 1):
         for j in range(len(steps_w) - 1):
-            rectangle = [steps_h[i], steps_w[j], steps_h[i + 1], steps_w[j + 1]]
-            print "\t".join([str(x) for x in rectangle])
+            # rectangle = [steps_h[i], steps_w[j], steps_h[i + 1], steps_w[j + 1]]
+            # print "\t".join([str(x) for x in rectangle])
+            rectangle = {"X1":steps_h[i], "Y1":steps_w[j], "X2":steps_h[i + 1], "Y2":steps_w[j + 1]}
+            for param in ["X1", "Y1", "X2", "Y2"]:
+                sys.stdout.write(param + ":" + str(rectangle[param]) + "\t")
+            sys.stdout.write("\n")
