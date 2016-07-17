be2be408fd8658cee0263733b05a657b1389afc0
fiqas <salimaire@gmail.com>
2016-07-17 20:24:34 +0200

    Usunieto stare pliki

--- run_text_extractor.py
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Czyszczenie xml, WAŻNE
-python bad_xml.py $1
-name_unclean=$1
-name=$(echo $name_unclean | cut -f 1 -d '.')
-clean="_clean.xml"
-echo "Created $name$clean"
-#Wyrzucanie cech tekstowych
-python text_extractor.py $name$clean
