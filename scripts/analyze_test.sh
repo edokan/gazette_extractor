b715055ed8fe8f315e50d4916c0297af7da9bd1f
Nefertari <s396289@pc22i>
2016-09-04 14:17:16 +0200

    added to analyze text features with LM

--- scripts/analyze_test.sh
+++ scripts/analyze_test.sh
@@ -18,9 +18,9 @@ do
        > $DIR/page_$i.graphic_features.vw
 
    #Extract text features
-   #python3 scripts/text_features_extractor.py -pc $DIR/page_$i.rect \
-       #< $DIR/page_$i.xml_cleaned \
-       #> $DIR/page_$i.text_features.vw
+   python3 scripts/text_features_extractor.py -pc $DIR/page_$i.rect \
+       < $DIR/page_$i.xml_cleaned \
+       > $DIR/page_$i.text_features.vw
 
    #Merge all page's features into one file.
 
@@ -28,6 +28,7 @@ do
    paste -d" "\
        $DIR/page_$i.rect \
        $DIR/page_$i.graphic_features.vw \
+       $DIR/page_$i.text_features.vw \
        | \
        sed "s,^,| ${DDJVU_NAME}.djvu $(cat $DIR/metadata.vw) PAGE:$i ,g" \
        > $DIR/page_$i.features.vw
