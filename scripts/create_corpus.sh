0db06c9ee045ed5cc15c62d3b0d75d8e208d1f26
Nefertari <s396289@pc22i>
2016-09-10 15:35:30 +0200

    resolved bug with creating corpus

--- scripts/create_corpus.sh
+++ scripts/create_corpus.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-INPUT_DJVU=$1
+INPUT_DJVU=$@
 
 for gazette_djvu in $INPUT_DJVU; do
     gazette_title=`basename $gazette_djvu .djvu`
+    echo "GAZETTE "$gazette_title
     if [ -f dev-0/$gazette_title".necro" ]; then
 	python3 scripts/create_corpus.py -fn dev-0/$gazette_title".necro" >> LM/corpus_necrologies.txt
     fi
