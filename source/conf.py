bf9a33c6fa950a57ce2f0caffacc0090d1b33ae6
Nefertari <s396289@pc22i>
2017-01-09 20:24:27 +0100

    auto doc

--- source/conf.py
+++ source/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon',
+#    'sphinxcontrib.napoleon',
     'rst2pdf.pdfbuilder'
 ]
 
